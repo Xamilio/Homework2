a4e4415725076c966864dcad8d089e8530e3dab7
Xamilio <119781901+Xamilio@users.noreply.github.com>
2023-04-26 14:01:17 +0300

    Update Homework2.cpp

--- Homework2/Homework2.cpp
+++ Homework2/Homework2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using std::cout;
 using std::endl;
 using std::cin;
@@ -6,78 +6,75 @@ using std::cin;
 int main()
 {
 
-    cout << "tast 1\n";
+   cout << "\n\n1. Write a program that finds the arithmetic mean of three real numbers.\n\n";
 
-    double a, b, c;
-    cout << "First number:";
-    cin >> a;
-    cout << "Second number:";
-    cin >> b;
-    cout << "Third number:";
-    cin >> c;
-    cout << ((a + b + c) / 3) << endl;
+   double a, b, c;
+   cout << "First number:";
+   cin >> a;
+   cout << "Second number:";
+   cin >> b;
+   cout << "Third number:";
+   cin >> c;
+   cout << "Result = " << ((a + b + c) / 3) << endl;
 
-    cout << "\n\n\n\ntast 2\n";
+   cout << "\n\n2. Write a program that finds the root of the linear equation ax + b = 0.\n\n";
 
-    double d, e, x;
-    cout << "First number:";
-    cin >> d;
-    cout << "Second number:";
-    cin >> e;
-    cout << "x = " << (-e / d) << endl;
+   double d, e, x;
+   cout << "Input A:";
+   cin >> d;
+   cout << "Input B:";
+   cin >> e;
+   cout << "X = " << (-e / d) << endl;
 
-    cout << "\n\n\n\ntask 3\n";
-    
-    double k, l, t;
-    cout << "Number:";
-    cin >> k;
-    cout << "Degree";
-    cin >> l;
-    t = 1;
-    for (int i = 0; i < l; i++)
-    {
-        t = t * k;
-    }
-    cout << t;
+   cout << "\n\n3. The user enters a number and degree. The program calculates the specified power\n\n";
 
-    cout << "\n\n\n\ntast 4\n";
-    double pi, r;
-    cout << "Radius: ";
-    cin >> r;
-    pi = 3.14;
-    cout << "Area " << (pi * (r * r)) << endl;
-    cout << "Length " << (pi * (r + r)) << endl;
+   double k, l, t;
+   cout << "Number:";
+   cin >> k;
+   cout << "Degree";
+   cin >> l;
+   t = 1;
+   for (int i = 0; i < l; i++)
+   {
+       t = t * k;
+   }
+   cout << "Result = " << t << endl;
+
+   cout << "\n\n4. Write a program that prompts the user to enter the radius of a circle and then calculates the area and length of that circle\n\n";
+   double pi, r;
+   cout << "Radius: ";
+   cin >> r;
+   pi = 3.14;
+   cout << "Area " << (pi * (r * r)) << endl;
+   cout << "Length " << (pi * (r + r)) << endl;
 
 
-    cout << "\n\n\n\ntast 5\n";
-    double h;
-    cout << "Hryvnia: ";
-    cin >> h;
-    cout << "dol " << (h * 0.027) << endl;
-    cout << "bit " << (h * 0.000001) << endl;
-    cout << "eur " << (h * 0.025) << endl;
-    
+   cout << "\n\n5. Write a program that allows the user to enter the number of hryvnias from the keyboard, and converts this amount into dollars, euros and bitcoins\n\n";
+   double h;
+   cout << "Hryvnia: ";
+   cin >> h;
+   cout << "dol " << (h * 0.027) << endl;
+   cout << "bit " << (h * 0.000001) << endl;
+   cout << "eur " << (h * 0.025) << endl;
 
-    cout << "\n\n\n\ntast 6\n";
-   
-    double kil;
-    cout << "Kilometers: ";
-    cin >> kil;
-    cout << "Statute miles " << (kil * 0.621371) << endl;
-    cout << "Nautical miles " << (kil * 0.539957) << endl;
 
-    cout << "\n\n\n\ntast 7\n";
+   cout << "\n\n6. Write a program that converts kilometers to land and nautical miles\n\n";
 
-    double chi, cho;
-    cout << "Number " << endl;
-    cin >> chi;
-    cout << "Percentage " << endl;
-    cin >> cho;
-    cout << (chi / 100) * cho << endl;
+   double kil;
+   cout << "Kilometers: ";
+   cin >> kil;
+   cout << "Statute miles " << (kil * 0.621371) << endl;
+   cout << "Nautical miles " << (kil * 0.539957) << endl;
 
-    cout << "\n\n\n\ntast 8\n";
+   cout << "\n\n7. Write a program that finds the percentage P of a number N.\n\n";
+
+   double chi, cho;
+   cout << "Number " << endl;
+   cin >> chi;
+   cout << "Percentage " << endl;
+   cin >> cho;
+   cout << "Result = " << (chi / 100) * cho << endl;    
 
 
 }
-
 
